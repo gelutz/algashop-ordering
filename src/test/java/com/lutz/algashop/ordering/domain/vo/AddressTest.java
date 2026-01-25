a4f610b14867beb949ef9a0798397b4a814d413c
gelutz <gabriel.lutz5@gmail.com>
2026-01-25 09:34:47 -0300

    feat: adds AddressTest

--- src/test/java/com/lutz/algashop/ordering/domain/vo/AddressTest.java
+++ src/test/java/com/lutz/algashop/ordering/domain/vo/AddressTest.java
@@ -1,5 +1,25 @@
 package com.lutz.algashop.ordering.domain.vo;
 
-class AddressTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+class AddressTest {
+	@Test
+	void givenBlankValuesConstructorShouldThrowIllegalArgumentException() {
+		ZipCode validZipCode = new ZipCode("123123");
+		Address sut = Address.builder()
+		                     .street("This street")
+		                     .number("102")
+		                     .complement("near the thing")
+		                     .neighborhood("that one")
+		                     .city("Brooklyn")
+		                     .state("New York")
+		                     .zipCode(validZipCode)
+		                     .build();
+		Assertions.assertThrows(IllegalArgumentException.class, () -> sut.toBuilder().street("").build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> sut.toBuilder().number("").build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> sut.toBuilder().neighborhood("").build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> sut.toBuilder().city("").build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> sut.toBuilder().state("").build());
+	}
 }
