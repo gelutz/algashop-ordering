d34b2730469805ea56a74a61d70304eef619f51a
gelutz <gabriel.lutz5@gmail.com>
2026-08-03 19:32:33 -0300

    config(exception-handler): removes redundant Nullable annotation

--- src/main/java/com/lutz/algashop/ordering/presentation/ApiExceptionHandler.java
+++ src/main/java/com/lutz/algashop/ordering/presentation/ApiExceptionHandler.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.*;
-import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,7 +25,6 @@ import java.util.stream.Collectors;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	private final MessageSource messageSource;
 
-	@Nullable
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 		ProblemDetail problemDetail = ProblemDetail.forStatus(status);
