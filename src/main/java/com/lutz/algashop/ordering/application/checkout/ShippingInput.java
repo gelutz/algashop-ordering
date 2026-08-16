0d899269ba1fdd47b2c835d0b6eb132de427c8c2
gelutz <gabriel.lutz5@gmail.com>
2026-08-15 21:52:51 -0300

    feat(checkout): adds bean validation to checkout input DTOs

--- src/main/java/com/lutz/algashop/ordering/application/checkout/ShippingInput.java
+++ src/main/java/com/lutz/algashop/ordering/application/checkout/ShippingInput.java
@@ -2,6 +2,8 @@ package com.lutz.algashop.ordering.application.checkout;
 
 import com.lutz.algashop.ordering.application.commons.AddressData;
 import com.lutz.algashop.ordering.application.order.query.RecipientData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShippingInput {
+	@Valid
+	@NotNull
 	private RecipientData recipient;
+
+	@Valid
+	@NotNull
 	private AddressData address;
 }
--- src/main/java/com/lutz/algashop/ordering/application/order/query/BillingData.java
+++ src/main/java/com/lutz/algashop/ordering/application/order/query/BillingData.java
@@ -1,6 +1,9 @@
 package com.lutz.algashop.ordering.application.order.query;
 
 import com.lutz.algashop.ordering.application.commons.AddressData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +14,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BillingData {
+	@NotBlank
 	private String firstName;
+
+	@NotBlank
 	private String lastName;
+
+	@NotBlank
 	private String document;
+
+	@NotBlank
 	private String phone;
+
+	@NotBlank
 	private String email;
+
+	@Valid
+	@NotNull
 	private AddressData address;
 }
